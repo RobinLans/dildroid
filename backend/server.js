811a718fd5cb752d818ed4a84ef1f943572dd5c7
Robin Lans <roblan95@icloud.com>
2021-09-13 11:10:01 +0200

    add backend

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const db = require("./database");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/api/register-user/", (req, res) => {
+  let user = req.body;
+
+  let insert = db.registerUser(user);
+  user.id = insert.lastInsertRowid;
+  res.json(user);
+});
+// Gets login credentials in the body and then checks if those credentials matches with any credentials in the database.
+// If there is a match, the response is { success: true }, if there is no match then the respnse is { success: false }
+app.post("/api/login", async (req, res) => {
+  console.log("hej");
+  const loginCredentials = req.body;
+
+  const userExists = await db.checkIfUserExists(loginCredentials);
+  let result = { success: false };
+
+  if (userExists > 0) {
+    result.success = true;
+  }
+
+  res.json(result);
+});
+
+app.listen(8000, () => {
+  console.log("Server started on port 8000");
+});
