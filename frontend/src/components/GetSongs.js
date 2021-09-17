f4af0ee246ca73f37fd8f282320c5557ad9c78d8
marcusrydell <marcusrydell@msn.com>
2021-09-17 10:22:11 +0200

    remove error

--- frontend/src/components/GetSongs.js
+++ frontend/src/components/GetSongs.js
@@ -8,129 +8,132 @@ import PlayerControls from "./PlayerControls";
 import style from "../styles/SearchResults.module.css";
 
 function GetSongs({ searchType, playlistId, inputValue }) {
-  const [musicList, setMusicList] = useState([]);
-  console.log(playlistId);
+    const [musicList, setMusicList] = useState([]);
+    console.log(playlistId);
 
-  async function fetchMusic() {
-    if (playlistId) {
-      const response = await fetch(`/playlist/${playlistId}`);
-      let result = await response.json();
-      console.log(result.songs);
-      setMusicList(result.songs);
-    } else if (inputValue) {
-      const response = await fetch(
-        `https://yt-music-api.herokuapp.com/api/yt/${searchType}/${inputValue}`
-      );
-      let result = await response.json();
-      console.log(result.content);
+    async function fetchMusic() {
+        if (playlistId) {
+            const response = await fetch(`/playlist/${playlistId}`);
+            let result = await response.json();
+            console.log(result.songs);
+            setMusicList(result.songs);
+        } else if (inputValue) {
+            const response = await fetch(
+                `https://yt-music-api.herokuapp.com/api/yt/${searchType}/${inputValue}`
+            );
+            let result = await response.json();
+            console.log(result.content);
 
-      setMusicList(result.content);
+            setMusicList(result.content);
+        }
     }
-  }
 
-  useEffect(() => {
-    fetchMusic();
-  }, [inputValue]);
+    useEffect(() => {
+        fetchMusic();
+    }, [inputValue]);
 
-  // If musicList is true this function will run (See line 116)
-  // The function maps through musicList and mounts SearchItem on every instance,
-  // it also sends down props that we can use later
-  function listSongs() {
-    return musicList.map((song, index) => (
-      <SongItem
-        {...song}
-        key={index}
-        index={index}
-        giveBackIndex={giveBackIndexAndStartPlaylist}
-      />
-    ));
-  }
+    // If musicList is true this function will run (See line 116)
+    // The function maps through musicList and mounts SearchItem on every instance,
+    // it also sends down props that we can use later
+    function listSongs() {
+        return musicList.map((song, index) => ( <
+            SongItem {...song }
+            key = { index }
+            index = { index }
+            giveBackIndex = { giveBackIndexAndStartPlaylist }
+            />
+        ));
+    }
 
-  //YT Player stuff
-  const [player, setPlayer] = useState();
-  const [paused, setPaused] = useState(false);
-  const [showControls, setShowControls] = useState(false);
-  const [duration, setDuration] = useState("");
-  const [animation, setAnimation] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [currentTime, setCurrentTime] = useState(0);
+    //YT Player stuff
+    const [player, setPlayer] = useState();
+    const [paused, setPaused] = useState(false);
+    const [showControls, setShowControls] = useState(false);
+    const [duration, setDuration] = useState("");
+    const [animation, setAnimation] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
 
-  function sendPlayerBack(player) {
-    setPlayer(player);
-  }
+    function sendPlayerBack(player) {
+        setPlayer(player);
+    }
 
-  //This function gets the index and time from SearchItem
-  function giveBackIndexAndStartPlaylist(index, time) {
-    const arrayOfVideoIds = musicList.map((song) => song.videoId);
-    setShowControls(true);
-    whenUnPause();
-    setDuration(time);
-    setCurrentIndex(index);
-    player.internalPlayer.loadPlaylist(arrayOfVideoIds, index);
-  }
+    //This function gets the index and time from SearchItem
+    function giveBackIndexAndStartPlaylist(index, time) {
+        const arrayOfVideoIds = musicList.map((song) => song.videoId);
+        setShowControls(true);
+        whenUnPause();
+        setDuration(time);
+        setCurrentIndex(index);
+        player.internalPlayer.loadPlaylist(arrayOfVideoIds, index);
+    }
 
-  function pausePlayer() {
-    player.internalPlayer.pauseVideo();
-    setPaused(true);
-    // setAnimation(false);
-  }
+    function pausePlayer() {
+        player.internalPlayer.pauseVideo();
+        setPaused(true);
+        // setAnimation(false);
+    }
 
-  //This function gets called in PlayerControls
-  function playPlayer() {
-    player.internalPlayer.playVideo();
-    whenUnPause();
-  }
+    //This function gets called in PlayerControls
+    function playPlayer() {
+        player.internalPlayer.playVideo();
+        whenUnPause();
+    }
 
-  //if we move the input slider in PlayerControls this function will run
-  function handleInputChange(e) {
-    player.internalPlayer.seekTo(e, true);
-    setCurrentTime(e);
-  }
+    //if we move the input slider in PlayerControls this function will run
+    function handleInputChange(e) {
+        player.internalPlayer.seekTo(e, true);
+        setCurrentTime(e);
+    }
 
-  function playNextVideoInPlaylist() {
-    player.internalPlayer.nextVideo();
-    whenUnPause();
-    const songLength = musicList[currentIndex + 1].duration;
-    setDuration(songLength);
-    if (currentIndex + 1 < musicList.length + 1)
-      setCurrentIndex(currentIndex + 1);
-  }
-  function playPreviuosVideoInPlaylist() {
-    player.internalPlayer.previousVideo();
-    whenUnPause();
-    const songLength = musicList[currentIndex - 1].duration;
-    setDuration(songLength);
-    if (currentIndex - 1 >= 0) setCurrentIndex(currentIndex - 1);
-  }
+    function playNextVideoInPlaylist() {
+        player.internalPlayer.nextVideo();
+        whenUnPause();
+        const songLength = musicList[currentIndex + 1].duration;
+        setDuration(songLength);
+        if (currentIndex + 1 < musicList.length + 1)
+            setCurrentIndex(currentIndex + 1);
+    }
 
-  function whenUnPause() {
-    setPaused(false);
-    setAnimation(true);
-  }
+    function playPreviuosVideoInPlaylist() {
+        player.internalPlayer.previousVideo();
+        whenUnPause();
+        const songLength = musicList[currentIndex - 1].duration;
+        setDuration(songLength);
+        if (currentIndex - 1 >= 0) setCurrentIndex(currentIndex - 1);
+    }
 
-  return (
-    <div>
-      <div className={style.videowrapper}>
-        <div className={style.videotextlist}></div>
-        <div className="searchResult">{musicList && listSongs()}</div>
-        <YouTubePlayer sendPlayerBack={sendPlayerBack} />
-        {showControls && (
-          <PlayerControls
-            pauseVideo={pausePlayer}
-            playVideo={playPlayer}
-            nextVideo={playNextVideoInPlaylist}
-            previousVideo={playPreviuosVideoInPlaylist}
-            paused={paused}
-            currentTime={currentTime}
-            duration={duration}
-            handleInputChange={handleInputChange}
-            animation={animation}
-            player={player}
-          />
-        )}
-      </div>
-    </div>
-  );
+    function whenUnPause() {
+        setPaused(false);
+        setAnimation(true);
+    }
+
+    return ( <
+        div >
+        <
+        div className = { style.videowrapper } >
+        <
+        div className = { style.videotextlist } > < /div> <
+        div className = "searchResult" > { musicList && listSongs() } < /div> <
+        YouTubePlayer sendPlayerBack = { sendPlayerBack }
+        /> {
+            showControls && ( <
+                PlayerControls pauseVideo = { pausePlayer }
+                playVideo = { playPlayer }
+                nextVideo = { playNextVideoInPlaylist }
+                previousVideo = { playPreviuosVideoInPlaylist }
+                paused = { paused }
+                currentTime = { currentTime }
+                duration = { duration }
+                handleInputChange = { handleInputChange }
+                animation = { animation }
+                player = { player }
+                />
+            )
+        } <
+        /div> <
+        /div>
+    );
 }
 
 export default GetSongs;
--- frontend/src/components/SongItem.js
+++ frontend/src/components/SongItem.js
@@ -3,55 +3,67 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faPlus, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 function SongItem(props) {
-  console.log(props);
-  let { name, artist, videoId, type, duration, title } = props;
+    console.log(props);
+    let { name, artist, videoId, type, duration, title } = props;
 
-  let multipleArtist = [];
-  let artistString = "";
+    let multipleArtist = [];
+    let artistString = "";
 
-  if (title) name = title;
-  if (Array.isArray(artist)) {
-    artist.map((a) => {
-      multipleArtist.push(a.name);
-    });
-    artistString = multipleArtist.toString();
-  }
+    //Handle if multiple artist collab on one song. Convert obj to string.
+    if (title) name = title;
+    if (Array.isArray(artist)) {
+        artist.map((a) => {
+            multipleArtist.push(a.name);
+        });
+        artistString = multipleArtist.toString();
+    }
 
-  let playList = [];
-  function saveToPlaylist(id) {
-    playList.push(id);
-  }
+    let playList = [];
 
-  return (
-    <>
-      <div>
-        <h4>{name}</h4>
-        {artist.name ? <p>{artist.name}</p> : <p>{artistString}</p>}
-        {typeof artist === "string" && <p>{artist}</p>}
-      </div>
-      <div className="buttons">
-        <button
-          onClick={() => {
-            localStorage.setItem("id", videoId);
-            props.giveBackIndex(props.index, duration);
-          }}
-        >
-          {<FontAwesomeIcon icon={faPlay} />}
-        </button>
+    function saveToPlaylist(id) {
+        playList.push(id);
+    }
 
-        <button onClick={() => saveToPlaylist(videoId)}>
-          {<FontAwesomeIcon icon={faPlus} />}
-        </button>
-        <button>{<FontAwesomeIcon icon={faHeart} />}</button>
-      </div>
+    return ( <
 
-      {/* {type === "artist" && (
-        <div>
-          <h4>{name}</h4>
-        </div>
-      )} */}
-    </>
-  );
-}
+            >
+            <
+            div >
+            <
+            h4 > { name } < /h4> { artist.name ? < p > { artist.name } < /p >: < p > { artistString } < /p > } {
+            typeof artist === "string" && < p > { artist } < /p>} < /
+            div > <
+            div className = "buttons" >
+            <
+            button onClick = {
+                () => {
+                    localStorage.setItem("id", videoId);
+                    props.giveBackIndex(props.index, duration);
+                }
+            } > { < FontAwesomeIcon icon = { faPlay }
+                />} < /
+                button >
 
-export default SongItem;
+                <
+                button onClick = {
+                    () => saveToPlaylist(videoId)
+                } > { < FontAwesomeIcon icon = { faPlus }
+                    />} < /
+                    button > <
+                    button > { < FontAwesomeIcon icon = { faHeart }
+                        />}</button >
+                        <
+                        /div>
+
+                        {
+                            /* {type === "artist" && (
+                                    <div>
+                                      <h4>{name}</h4>
+                                    </div>
+                                  )} */
+                        } <
+                        />
+                    );
+                }
+
+                export default SongItem;
